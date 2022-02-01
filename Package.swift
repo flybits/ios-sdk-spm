c2423b109bf91762b474e364d52bfa85accc2de1
Distiller <distiller@static.38.39.186.181.cyberlynk.net>
2022-02-01 19:43:55 +0000

    Build: New: create spm support for 4.0

--- Package.swift
+++ Package.swift
@@ -5,18 +5,17 @@ import PackageDescription
 let package = Package(
   name: "Flybits",
   products: [
-    .library(name: "Concierge", targets: ["FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK", "FlybitsConciergeSDK"]),
+    .library(name: "Concierge", targets: ["FlybitsCoreConcierge", "FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK", "FlybitsConcierge"]),
     .library(name: "Location", targets: ["FlybitsSDK", "FlybitsContextSDK", "FlybitsContextLocationPluginSDK"]),
     .library(name: "Core", targets: ["FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK"]),
-    .library(name: "SmartRewards", targets: ["FlybitsSDK", "FlybitsPushSDK", "FlybitsKernelSDK", "FlybitsContextSDK", "FlybitsConciergeSDK", "FlybitsSmartRewardsSDK"]),
   ],
   targets: [
-    .binaryTarget(name: "FlybitsSDK", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/3.8.1/FlybitsSDK.xcframework.zip", checksum: "ebdbf9f02b96ce1af598d8386c74af688067b7235b9d02679c749c02fb4bda5e"),
-    .binaryTarget(name: "FlybitsPushSDK", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/3.8.1/FlybitsPushSDK.xcframework.zip", checksum: "bf59172efbbbe4cf3f77dfbb2a12b79a54b7defab97fdc4985befb87168e573f"),
-    .binaryTarget(name: "FlybitsKernelSDK", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/3.8.1/FlybitsKernelSDK.xcframework.zip", checksum: "08ccefa8313edd14797fa853b5137dd601da21a2d366abaf7b61bb37354bfc99"),
-    .binaryTarget(name: "FlybitsContextSDK", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/3.8.1/FlybitsContextSDK.xcframework.zip", checksum: "aaa01f8f52331d75ef18c89e5b214bbe98799b572057be000502310c6e44e76c"),
-    .binaryTarget(name: "FlybitsConciergeSDK", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/3.8.1/FlybitsConciergeSDK.xcframework.zip", checksum: "1a5e88cd4ba5b2b4db914b634b81bbc5a802dd688a22cb3fa3798ff2f234ebe0"),
-    .binaryTarget(name: "FlybitsSmartRewardsSDK", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/3.8.1/FlybitsSmartRewardsSDK.xcframework.zip", checksum: "eb5195b665c285cf8919a2af201f8fb98420105472217f3db3dc5495ef9c3552"),
-    .binaryTarget(name: "FlybitsContextLocationPluginSDK", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/3.8.1/FlybitsContextLocationPluginSDK.xcframework.zip", checksum: "d88cdb660542f43114141edb3f9047564bf4a9d865e05a74297312e90aada64c"),
+    .binaryTarget(name: "FlybitsSDK", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/4.0.0/FlybitsSDK.xcframework.zip", checksum: "b6564aa051edb34ba5253bfae5ed9d5930696fd97a0a50c3164b3368f7829532"),
+    .binaryTarget(name: "FlybitsPushSDK", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/4.0.0/FlybitsPushSDK.xcframework.zip", checksum: "387d7d1d29320b3f43f7eb379a81a92458bae981d5ae79039d7e5390ba235916"),
+    .binaryTarget(name: "FlybitsKernelSDK", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/4.0.0/FlybitsKernelSDK.xcframework.zip", checksum: "0e040d96b23558a274096c361f765e814e40d15a3ffda94d4701f58fe7ca713a"),
+    .binaryTarget(name: "FlybitsContextSDK", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/4.0.0/FlybitsContextSDK.xcframework.zip", checksum: "868d1ba371e961d9d4fd14a3a533240368ae3b380bdd19456b00e769a965f73f"),
+    .binaryTarget(name: "FlybitsCoreConcierge", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/4.0.0/FlybitsCoreConcierge.xcframework.zip", checksum: "c586f26f5164fd034c93516b8f8cb243aa00af1e29e7394efa27c5b826597bf9"),
+    .binaryTarget(name: "FlybitsConcierge", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/4.0.0/FlybitsConcierge.xcframework.zip", checksum: "31853f2ae15dbe5d3f96ae8b82888af3451c03eb3209fbaabb66f5addcd10a13"),
+    .binaryTarget(name: "FlybitsContextLocationPluginSDK", url: "https://flybits.jfrog.io/artifactory/SPM/Flybits/4.0.0/FlybitsContextLocationPluginSDK.xcframework.zip", checksum: "f4f1c93c7878b2813792028c5f2e15bf96fcb8b7c4451a7023611569b335ea27"),
   ]
 )
